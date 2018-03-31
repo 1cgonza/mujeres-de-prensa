6e21dd216872903ac5fdb758103fb532c851e47b
Juan Camilo González <info@juancgonzalez.com>
2018-03-31 12:57:37 -0500

    implement theme section

--- app.js
+++ app.js
@@ -7,8 +7,50 @@ var apos = require('apostrophe')({
       viewsFolderFallback: path.join(__dirname, 'views')
     },
     'profiles-widgets': {},
+    'magazines-widgets': {},
     'link-widgets': {},
     'page-link-widgets': {},
-    'illustrations-widgets': {}
+    'illustrations-widgets': {},
+    'theme-pages': {
+      extend: 'apostrophe-custom-pages',
+      name: 'seccion-tematica',
+      arrangeFields: [
+        {
+          name: 'sectionConfiguration',
+          label: 'Section configuration',
+          fields: ['themeColor', 'themeType']
+        }
+      ],
+      addFields: [
+        {
+          name: 'themeColor',
+          type: 'color',
+          label: 'Theme Color'
+        },
+        {
+          type: 'select',
+          name: 'themeType',
+          label: 'Section',
+          choices: [
+            {
+              label: 'La mujer tiene Derecho a intervenir',
+              value: 'calle'
+            },
+            {
+              label: 'Lo que vemos las mujeres',
+              value: 'mesa'
+            },
+            {
+              label: 'Cuéntame tu vida',
+              value: 'casa'
+            },
+            {
+              label: 'Toda obra tiene su principio',
+              value: 'taller'
+            }
+          ]
+        }
+      ]
+    }
   }
 });

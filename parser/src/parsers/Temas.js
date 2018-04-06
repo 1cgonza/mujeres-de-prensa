dc70ebf6f5bda2bd18733aaa0b0fe1a6e80c70b6
Juan Camilo González <info@juancgonzalez.com>
2018-04-06 14:08:31 -0500

    implement fuzzy string comparison

--- parser/src/parsers/Temas.js
+++ parser/src/parsers/Temas.js
@@ -2,105 +2,178 @@ import Base from './Base';
 import { isArray } from 'util';
 import { secondaryUrls } from '../utils/data';
 import req from '../utils/req';
+import fuzz from 'fuzzball';
 
 export default class Temas extends Base {
   constructor(tableName) {
     super(tableName);
     this.vols = [];
     this.categories = {};
+    this.editions = {};
   }
 
   clean() {
-    this.getThemes();
-    let cleanedData = this.sortAndFilter(this.getBaseArray(this.data.raw));
-    this.getVols(this.vols, this.totalRows);
+    return new Promise((resolve, reject) => {
+      this.getThemes();
+      let cleanedData = this.sortAndFilter(this.getBaseArray(this.data.raw));
+      this.getVols(this.vols, this.totalRows);
 
-    cleanedData = cleanedData.map(ref => {
-      let cell = this.data.raw[ref.key];
-      let titlePages = this.getTitleAndPages(cell.v, ref.key);
-      let ret = {
-        title: titlePages.title,
-        category: this.categories[ref.col]
-      };
+      cleanedData = cleanedData.map(ref => {
+        let cell = this.data.raw[ref.key];
+        let titlePages = this.getTitleAndPages(cell.v, ref.key);
+        let ret = {
+          title: titlePages.title,
+          category: this.categories[ref.col]
+        };
 
-      if (titlePages.hasOwnProperty('pages')) {
-        ret.pages = titlePages.pages;
-      }
+        if (titlePages.hasOwnProperty('pages')) {
+          ret.pages = titlePages.pages;
+        }
 
-      if (cell.s.hasOwnProperty('fgColor')) {
-        let theme = this.themes.find(el => el.color === cell.s.fgColor.rgb);
+        if (cell.s.hasOwnProperty('fgColor')) {
+          let theme = this.themes.find(el => el.color === cell.s.fgColor.rgb);
 
-        if (theme) {
-          ret.theme = theme.name;
-        } else {
+          if (theme) {
+            ret.theme = theme.name;
+          } else {
+            this.setError({
+              error: `Background color of cell ${ref.key} does not match any theme`,
+              colorDataFound: cell.s
+            });
+          }
+        }
+
+        let meta = this.vols.find(obj => ref.row >= obj.rows[0] && ref.row <= obj.rows[1]);
+
+        if (!meta) {
           this.setError({
-            error: `Background color of cell ${ref.key} does not match any theme`,
-            colorDataFound: cell.s
+            stop: true,
+            error: {
+              error: `Can't match edition number of cell ${ref.key} to anything`
+            }
           });
         }
-      }
 
-      let meta = this.vols.find(obj => ref.row >= obj.rows[0] && ref.row <= obj.rows[1]);
-
-      if (!meta) {
-        this.setError({
-          stop: true,
-          error: {
-            error: `Can't match edition number of cell ${ref.key} to anything`
-          }
-        });
-      }
-
-      Object.assign(ret, meta);
-      // Object.assign(ret, ref);
-
-      return ret;
-    });
-
-    req(secondaryUrls.generos).then(d => {
-      if (d.hasOwnProperty('Sheets') && d.Sheets.hasOwnProperty(this.sheetName)) {
-        let generosData = d.Sheets[this.sheetName];
-        let finalRow = this.getFinalRow(generosData);
-        let baseArr = this.getBaseArray(generosData);
-        let vols = [];
-        let generos = [];
-
-        let cleaned = this.sortCells(baseArr).filter(obj => {
-          if (obj.col === 'A') {
-            if (obj.row !== 1) {
-              let v = generosData[obj.key].v;
-              let ed = !isNaN(v) ? +v : this.validatePages(v);
-              vols.push(Object.assign(obj, {
-                ed: ed
-              }));
+        Object.assign(ret, ref, meta);
+        // Object.assign(ret, ref);
+        if (!this.editions.hasOwnProperty(`ed${ret.ed}`)) {
+          this.editions[`ed${ret.ed}`] = [ret];
+        } else {
+          let matchArticle = this.editions[`ed${ret.ed}`].find((article, i) => {
+            if (article.ed === ret.ed && fuzz.ratio(article.title, ret.title) > 95) {
+              if (isArray(article.category)) {
+                this.editions[`ed${ret.ed}`].category.push(ret.category);
+              } else {
+                this.editions[`ed${ret.ed}`].category = [article.category, ret.category];
+              }
+              console.log('repeated title', article, ret);
             }
+
             return false;
-          } else if (obj.row === 1) {
-            generos[obj.col] = generosData[obj.key].v;
-            return false;
-          }
-          return true;
-        });
+          });
 
-        this.getVols(vols, finalRow);
+          this.editions[`ed${ret.ed}`].push(ret);
+        }
 
-        cleaned = cleaned.map(ref => {
-          let cell = generosData[ref.key];
-          console.log(cell, ref);
-          let titlePages = this.getTitleAndPages(cell.v, ref.key);
-          let ret = {
-            title: titlePages.title,
-          };
-          
+        
+        // if (this.editions[`ed${ret.ed}`].title === ret.title) {
+        //   
+        // }
 
-        });
+        return ret;
+      });
+      console.log(this.editions);
 
-        console.log(cleaned, vols, generos);
-        // console.log(cleanedData, this.vols, this.categories);
+      req(secondaryUrls.generos).then(d => {
+        if (d.hasOwnProperty('Sheets') && d.Sheets.hasOwnProperty(this.sheetName)) {
+          let generosData = d.Sheets[this.sheetName];
+          let finalRow = this.getFinalRow(generosData);
+          let baseArr = this.getBaseArray(generosData);
+          let vols = [];
+          let genres = [];
+          let titles = {};
 
-      }
+          let cleaned = this.sortCells(baseArr).filter(obj => {
+            if (obj.col === 'A') {
+              if (obj.row !== 1) {
+                let v = generosData[obj.key].v;
+                let ed = !isNaN(v) ? +v : this.validatePages(v);
+                vols.push(Object.assign(obj, {
+                  ed: ed
+                }));
+              }
+              return false;
+            } else if (obj.col === 'B') {
+              if (obj.row !== 1) {
+                titles[`row${obj.row}`] = this.getTitleAndPages(generosData[obj.key].v, obj.key);
+              }
+              return false;
+            } else if (obj.row === 1) {
+              genres[obj.col] = generosData[obj.key].v;
+              return false;
+            } else if (generosData[obj.key].v === 0) {
+              return false;
+            }
+
+            return true;
+          });
+
+          this.getVols(vols, finalRow);
+
+          cleaned = cleaned.map(ref => {
+            let cell = generosData[ref.key];
+            let ret = {
+              genre: genres[ref.col],
+              title: titles[`row${ref.row}`].title
+            };
+
+            let meta = vols.find(obj => ref.row >= obj.rows[0] && ref.row <= obj.rows[1]);
+
+            if (!meta) {
+              this.setError({
+                stop: true,
+                error: {
+                  error: `Can't match edition number of cell ${ref.key} to anything`
+                }
+              });
+            }
+
+            Object.assign(ret, ref, meta);
+
+            return ret;
+          });
+
+          console.log(cleaned);
+
+          cleanedData.forEach(d => {
+            let match = cleaned.find(genre => {
+              let ratio = fuzz.ratio(genre.title, d.title);
+
+              if (ratio === 100) {
+                return true;
+              } else if (ratio > 80) {
+                this.setError({
+                  error: {
+                    warning: `These titles seem to match`,
+                    Generos: `${genre.title}" - ${genre.key}`,
+                    Titulos: `${d.title}" - ${d.key}`
+                  }
+                });
+                //console.log(genre.title, d.title);
+              }
+
+              return false;
+            });
+
+            // if (!match) {
+            //   console.log(d);
+            // }
+          });
+
+          resolve();
+        }
+      });
     });
-
   }
 
   sortAndFilter(colsRowsArr) {

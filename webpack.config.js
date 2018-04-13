15971baffec36a3f3fbe81067f96e717005a31af
Juan Camilo González <info@juancgonzalez.com>
2018-04-12 22:40:11 -0500

    use webpack 4 optimized mode for dev & prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,7 @@
 const path = require('path');
-const webpack = require('webpack');
 const dist = path.join(__dirname, 'lib/modules/apostrophe-assets/public');
 
 module.exports = {
-  devtool: 'eval-source-map',
-  watch: true,
-  watchOptions: {
-    ignored: /node_modules/
-  },
   entry: ['./src/index.js'],
   output: {
     path: dist,
